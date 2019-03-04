02211b19bb14b5ef83e8d26f06b1a8990fbc67ab
Jindřich Horváth <jindrich_horvath@kb.cz>
2019-03-04 13:27:32 +0100

    swipeable drawer

--- web/src/app/layouts/Layout3.js
+++ web/src/app/layouts/Layout3.js
@@ -3,16 +3,16 @@ import {withStyles} from "@material-ui/core/styles";
 import {menuMaxWidth, menuWidth} from "../themes/themeVariables";
 import AppMenu from "../appMenu/AppMenu";
 import AppContent from "../appContent/AppContent";
-import {Drawer, IconButton, Paper} from "@material-ui/core";
+import {Drawer, SwipeableDrawer, IconButton, Paper} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import Hidden from "@material-ui/core/Hidden";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
 import clsx from "clsx";
 import {Fragment} from "react";
 import {withRouter} from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
     root: {
@@ -22,19 +22,15 @@ const styles = theme => ({
     appBar: {
         zIndex: theme.zIndex.drawer + 1
     },
-    toolbarMain: {
-        display: 'flex',
-        //flexGrow: 1,  //takes up remaining width
+    logo: {
         width: menuWidth,
         alignSelf: 'stretch',
-        alignItems: 'center',
-        cursor: 'pointer',
-        marginLeft: {
-
-        },
-        [theme.breakpoints.up('sm')]: { //not mobile
-            ...theme.mixins.gutters()
-        }
+        borderRadius: 0,
+        ...theme.mixins.gutters()
+    },
+    logoLabel: {
+        justifyContent: 'flex-start',
+        textTransform: 'none'
     },
     drawer: {
         width: menuWidth,
@@ -43,33 +39,31 @@ const styles = theme => ({
     drawerPaper: {
         width: menuWidth,
     },
-    mobileDrawer: {
-    },
+    mobileDrawer: {},
     content: {
         height: '100%',
         display: 'flex',
         flexDirection: 'column',
         flexGrow: 1  //takes up remaining width
     },
-    contentPaper: {
+    contentArea: {
         flexGrow: 1, //takes up remaining height
         padding: theme.spacing.unit * 2,
         [theme.breakpoints.up('sm')]: {
             width: '90%',
             margin: 'auto',
         },
-        [theme.breakpoints.down('xs')]: {
-        }
+        [theme.breakpoints.down('xs')]: {}
     },
     toolbar: theme.mixins.toolbar
 });
 
-const appMenu = <AppMenu dense />;
+const appMenu = <AppMenu dense/>;
 
-const Layout3 = withRouter(withStyles(styles, {withTheme: true})(props => {
-    const {classes, theme, history} = props;
+const Layout3 = withRouter(withStyles(styles/*, {withTheme: true}*/)(props => {
+    const {classes, history} = props;
 
-    const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+//    const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
 
     const [open, setOpen] = useState(false);
 
@@ -92,11 +86,12 @@ const Layout3 = withRouter(withStyles(styles, {withTheme: true})(props => {
                             {<MenuIcon color="secondary"/>}
                         </IconButton>
                     </Hidden>
-                    <div className={classes.toolbarMain} onClick={navigateHome}>
-                        <Typography variant="h6" align={isMobile ? 'inherit' : 'center'}>
+                    <Button className={classes.logo} classes={{label: classes.logoLabel}}
+                            onClick={navigateHome}>
+                        <Typography variant="h6">
                             Chimera
                         </Typography>
-                    </div>
+                    </Button>
                 </Toolbar>
             </AppBar>
 
@@ -110,14 +105,14 @@ const Layout3 = withRouter(withStyles(styles, {withTheme: true})(props => {
                             paper: classes.drawerPaper
                         }}
                         variant="persistent">
-                        <div className={classes.toolbar} />
+                        <div className={classes.toolbar}/>
                         {appMenu}
                     </Drawer>
                 </Hidden>
 
                 <Hidden smUp implementation="css">
-                    <Drawer
-                        ModalProps={{ keepMounted: true }}
+                    <SwipeableDrawer
+                        ModalProps={{keepMounted: true}}
                         className={clsx(classes.drawer, classes.mobileDrawer)}
                         classes={{
                             paper: classes.drawerPaper
@@ -127,12 +122,12 @@ const Layout3 = withRouter(withStyles(styles, {withTheme: true})(props => {
                         open={open}
                         onClose={onClose}>
                         {appMenu}
-                    </Drawer>
+                    </SwipeableDrawer>
                 </Hidden>
 
                 <div className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <Paper className={classes.contentPaper}>
+                    <div className={classes.toolbar}/>
+                    <Paper square className={classes.contentArea}>
                         <AppContent/>
                     </Paper>
                 </div>
--- web/src/app/themes/themeVariables.js
+++ web/src/app/themes/themeVariables.js
@@ -1,3 +1,2 @@
-export const maxZIndex = 9999;
 export const menuWidth = 230;
-export const menuMaxWidth = "70%";
+export const menuMaxWidth = "85%";
